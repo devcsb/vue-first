797b079ca66d9e1d4e2015942fb9c237074778b4
choisoobong <devcsb119@gmail.com>
2022-06-13 11:02:34 +0900

    Vue CLI  Vue CLI 사용법과 싱글 파일 컴포넌트 소개

---
+++ vue-cli/src/Test.vue
@@ -0,0 +1,19 @@
+<!--HTML-->
+<template>
+  <div>header</div>
+</template>
+
+<script>
+// JavaScript - 인스턴스
+export default {
+  methods: {
+    addNum: function() {
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+/* CSS */
+</style>
