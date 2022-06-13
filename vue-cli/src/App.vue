838e79ce4bb9bbabdb3dfb4b6057fe67e1227d8c
choisoobong <devcsb119@gmail.com>
2022-06-13 16:02:11 +0900

    싱글 파일 컴포넌트
    싱글 파일 컴포넌트에서 props, event emit 사용법 숙지

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div><!-- template root는 무조건 하나의 element만 있어야 한다.-->
+<!--    <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header>-->
+    <app-header v-bind:propsdata="str"
+                v-on:renew="renewStr"></app-header>
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader"; // 하위 컴포넌트를 파일로 가져와서 APpHeader Component 정의
+
+// new Vue({
+//   data: {
+//     str: 'hi'
+//   }
+// })
+
+export default {
+  data: function () { // data는 익명함수에 return으로 새 객체를 반환하는 방식으로 컴포넌트 속성 정의한다
+    return {
+      str: 'Header'
+    }
+  },
+  components:{
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function (){
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-cli/src/main.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import App from './App.vue' // 1. 컴포넌트의 내용을 다른 파일에서 갸져온다. App.vue의 내용을 들고와서 App 변수에 저장
+
+Vue.config.productionTip = false
+
+// var App = { // 컴포넌트 내용 정의
+//   template: '<div>app</div>'
+// }
+
+new Vue({
+  render: h => h(App), // 여기서 render() 한다.
+  // 위 코드(render)와 아래 코드(컴포넌트 매핑)의 역할은 동일하다
+
+  // components: {
+  //   'app': App
+  // }
+
+}).$mount('#app')
+
+//$mount('#app') 대신 아래와 같이 el 로 정의하면 같은 동작을 한다.
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
